8d7a67695c197fd5e812e673e2fe2462d7806c8d
Qasim-Shaheen <qasim.shaheen.sh@gmail.com>
2026-01-02 23:52:32 +0100

    Refactor input prompts and output messages
    Updated input prompts and output messages for clarity.

--- Simulation Sales and Operations Planning (SOP).py
+++ Simulation Sales and Operations Planning (SOP).py
@@ -3,12 +3,12 @@ sq = []
 pq = []
 
 
-Stock_lvl = int(input("Please enter the initial stock level:"))
-num_months = int(input("Please enter the number of months to plan:"))
+Stock_lvl = int(input("Please enter an initial stock level: "))
+num_months = int(input("Please enter the number of months to plan: "))
 new_stock_lvl.append(Stock_lvl)
 
 for i in range(num_months):
-    sales_quantity = int(input("Please enter the expected monthly sales quantity:"))  
+    sales_quantity = int(input("Please enter the planned sales quantity: "))  
     sq.append(sales_quantity)
     
     if new_stock_lvl[i] >= sq[i]:
@@ -20,10 +20,11 @@ for i in range(num_months):
         pq.append(sq[i]- new_stock_lvl[i])
         new_stock_lvl.append(0)
 print("")
-
+print("The resulting production quantities are: ")
 for i in range(num_months):
     x = i +1
-    print("production quantity is month:",x ,"-" ,pq[i] )
+    print("production quantity is month: ",x ,"-" ,pq[i] )
+
 
 
 
